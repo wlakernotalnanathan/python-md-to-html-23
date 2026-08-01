b8b86b56ba63f9a809537f1cb8afa6058731adee
wlakernotalnanathan <25@felix-au.me>
2026-08-02 04:18:15 +0530

    Update parser.py - Step 7

--- parser.py
+++ parser.py
@@ -1,3 +1,7 @@
 def parse(text):
     lines = text.splitlines()
     res = []
+    for l in lines:
+        if l.startswith('# '): res.append(f'<h1>{l[2:]}</h1>')
+        else: res.append(f'<p>{l}</p>')
+    return '\n'.join(res)
